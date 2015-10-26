74142c0586d6164c3937792d37bc68e051d3fcd9
Eugene Butusov <EvgenyButusov@gmail.com>
2015-10-26 18:13:42 +0300

    IMPROVE 24 Check latest version in Unity Settings panel

--- Soomla/Assets/Plugins/Soomla/Levelup/Config/LevelUpSettings.cs
+++ Soomla/Assets/Plugins/Soomla/Levelup/Config/LevelUpSettings.cs
@@ -66,7 +66,7 @@ namespace Soomla.Levelup
 
 		public void OnInfoGUI() {
 			SoomlaEditorScript.SelectableLabelField(profileVersion, "1.0.17");
-			SoomlaEditorScript.LatestVersionField ("unity3d-levelup", "1.0.17");
+			SoomlaEditorScript.LatestVersionField ("unity3d-levelup", "1.0.17", "New LevelUp version available!", "http://library.soom.la/fetch/unity3d-levelup/latest?cf=unity");
 			EditorGUILayout.Space();
 		}
 
